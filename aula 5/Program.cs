719ef1cb74d303ba98047faa2c84b7ebe333a935
ALUNO <ALUNO@LABFPC36>
2024-02-26 08:50:42 -0300

    Adicionar arquivos de projeto.

---
+++ aula 5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           float a;
+          
+            a = int.Parse(Console.ReadLine());
+            float b = a / 4;
+            if (a == (int)b * 4)          
+            {
+                Console.WriteLine("numero invalido");
+            }
+            else
+            {
+                Console.WriteLine("numero valido");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
